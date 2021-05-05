ee636c5357b442456b46b978efb2711b0a58acd3
BlasToth <balazstothlaszlo+github@gmail.com>
2021-05-05 14:23:42 +0200

    add react-typed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Typed from 'react-typed';
 
 function Header() {
     return (
         <div>
             <h2>My name is Balázs Tóth (Blas) and I am a full-stack developer</h2>
+            <Typed 
+                className="type-text"
+                strings={"Web Development", "Google Ads", "Dynamic websites"}
+                typeSpeed={40}
+                backSpeed={60}
+                loop
+            />
         </div>
     )
 }
