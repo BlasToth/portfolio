eeb998123be6f897180d133bc18ef133952251a1
BlasToth <balazstothlaszlo+github@gmail.com>
2021-05-06 09:48:56 +0200

    build particles

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
-
+import ParticleBackground from './components/ParticleBackground'
 function App() {
   return (
     <>
+    <ParticleBackground />
     <Navbar />
     <Header />
     </>
